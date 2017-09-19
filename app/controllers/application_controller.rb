7faab6c25ce5f29eff541176610520a4aeff08df
YOUR NAME <YOUR@EMAIL.com>
2017-09-19 20:56:24 +0200

    Updated registered feature with username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   layout "application"
   protect_from_forgery with: :exception
+
+  before_action :configure_permited_params, if: :devise_controller?
+
+  def configure_permited_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
