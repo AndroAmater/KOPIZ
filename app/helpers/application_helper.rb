7f945231bf972cb971db61b736abb27488953139
YOUR NAME <YOUR@EMAIL.com>
2017-09-27 14:34:12 +0200

    App helper cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,4 @@
 module ApplicationHelper
-	def sort_column_gen (column, title = nil)
-		title ||= column.titleize
-		link_to title, {:sort => @active_column, :direction => @sort_direction}, {:class => "sort-column"}
-	end
-
 	def login_items
 		if current_user.nil?
 			("<div class = 'login_items'>".html_safe) +
