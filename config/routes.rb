1207f3ee1e3005b50b05de0d5a54ae3112179a8c
YOUR NAME <YOUR@EMAIL.com>
2017-09-27 14:12:27 +0200

    Removed unneccesary resources to make code DRY

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }
   root to: "projects#index"
+  resources :tasks
   resources :projects
   resources :project_phases
   get 'project_phases/create'
 
   get 'projects/create'
 
-  resources :tasks, :project_phases, :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
