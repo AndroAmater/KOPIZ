6d5e3c3dce21aef6d498984eb3af4c6580542378
YOUR NAME <YOUR@EMAIL.com>
2017-09-27 22:01:23 +0200

    Ran rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916164228) do
+ActiveRecord::Schema.define(version: 20170927195633) do
 
   create_table "contexts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "context_id"
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
   create_table "priorities", force: :cascade do |t|
@@ -31,7 +43,9 @@ ActiveRecord::Schema.define(version: 20170916164228) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "slug"
     t.index ["project_id"], name: "index_project_phases_on_project_id"
+    t.index ["slug"], name: "index_project_phases_on_slug", unique: true
     t.index ["user_id"], name: "index_project_phases_on_user_id"
   end
 
@@ -41,6 +55,8 @@ ActiveRecord::Schema.define(version: 20170916164228) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "slug"
+    t.index ["slug"], name: "index_projects_on_slug", unique: true
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
@@ -83,10 +99,12 @@ ActiveRecord::Schema.define(version: 20170916164228) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "slug"
     t.index ["context_id"], name: "index_tasks_on_context_id"
     t.index ["priority_id"], name: "index_tasks_on_priority_id"
     t.index ["project_phase_id"], name: "index_tasks_on_project_phase_id"
     t.index ["review_period_id"], name: "index_tasks_on_review_period_id"
+    t.index ["slug"], name: "index_tasks_on_slug", unique: true
     t.index ["status_id"], name: "index_tasks_on_status_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
