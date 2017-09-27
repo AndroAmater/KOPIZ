33a77826417f6310c3212ff92f26dd4390dc7c53
YOUR NAME <YOUR@EMAIL.com>
2017-09-27 13:28:46 +0200

    Prettyfied login_items helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,12 +7,14 @@ module ApplicationHelper
 	def login_items
 		if current_user.nil?
 			("<div class = 'login_items'>".html_safe) +
+				(link_to "Login", new_user_session_path)+
+				+ (" / ".html_safe) +
 				(link_to "Register", new_user_registration_path) +
-				 + (" / ".html_safe) +
-				(link_to "Login", new_user_session_path) +
 			("</div>".html_safe)
 		else
-			link_to "Logout", destroy_user_session_path, method: :delete
+			("<div class = 'login_items'>".html_safe) +
+				(link_to "Logout", destroy_user_session_path, method: :delete) +
+			("</div>".html_safe)
 		end
 	end
 end
