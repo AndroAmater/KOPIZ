c981ad936f41c207062a86af57e8a95e6a7dcca9
YOUR NAME <YOUR@EMAIL.com>
2017-09-27 13:25:14 +0200

    Implemented login/logout/register buttons into navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
 		title ||= column.titleize
 		link_to title, {:sort => @active_column, :direction => @sort_direction}, {:class => "sort-column"}
 	end
+
+	def login_items
+		if current_user.nil?
+			("<div class = 'login_items'>".html_safe) +
+				(link_to "Register", new_user_registration_path) +
+				 + (" / ".html_safe) +
+				(link_to "Login", new_user_session_path) +
+			("</div>".html_safe)
+		else
+			link_to "Logout", destroy_user_session_path, method: :delete
+		end
+	end
 end
