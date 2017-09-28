1b2129c271243b12897dfdbade1f2fa045e1edd7
YOUR NAME <YOUR@EMAIL.com>
2017-09-28 21:31:10 +0200

    no message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927213358) do
+ActiveRecord::Schema.define(version: 20170927195633) do
 
   create_table "contexts", force: :cascade do |t|
     t.datetime "created_at", null: false
